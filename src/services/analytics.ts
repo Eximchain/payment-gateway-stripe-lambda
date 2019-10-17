7bcd81af26b3510a87d4cc2ad3d8b7eb2fcd4e16
John O'Sullivan <john@eximchain.com>
2019-10-17 18:23:35 -0400

    now have console.log()s for debugging

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -3,16 +3,20 @@ import Analytics from 'analytics-node';
 
 export function identifyUserWithMetadata(email:string, traits:Record<string, any>) {
   if (typeof segmentWriteKey !== 'string') return;
+  console.log(`Segment key is present, attempting to track ${email} with ${JSON.stringify(traits, null, 2)}`)
+  const userId = email;
   const analytics = new Analytics(segmentWriteKey, { flushAt: 1 });
-  analytics.track({
-    event: 'User Signup - Server Confirmation',
-    userId: email
+  analytics
+    .track({
+      userId, 
+      event: 'User Signup - Server Confirmation',
+      properties: { ...traits }
   })
-  analytics.identify({
-    userId: email,
-    traits
+    .identify({
+      userId, traits
   })
   analytics.flush();
+  console.log('Tracking allegedly complete');
 }
 
 export default { identifyUserWithMetadata }
