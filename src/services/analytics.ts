cb47865f24e56c83bc0bffb18be0f4e7148abcd2
John O'Sullivan <john@eximchain.com>
2019-10-17 16:22:59 -0400

    Added additional track event for debugging

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -4,10 +4,15 @@ import Analytics from 'analytics-node';
 export function identifyUserWithMetadata(email:string, traits:Record<string, any>) {
   if (typeof segmentWriteKey !== 'string') return;
   const analytics = new Analytics(segmentWriteKey, { flushAt: 1 });
+  analytics.track({
+    event: 'User Signup - Server Confirmation',
+    userId: email
+  })
   analytics.identify({
     userId: email,
     traits
   })
+  analytics.flush();
 }
 
 export default { identifyUserWithMetadata }
