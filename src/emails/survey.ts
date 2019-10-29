0735ac39f513a6b9ea64c48dd0318a4497bf00e2
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-10-29 17:53:45 -0400

    Revert "Source survey email image from Google Drive"
    This reverts commit 389059c6e44cdbc1322b5f19d9762368c1e0ead6.

--- src/emails/survey.ts
+++ src/emails/survey.ts
@@ -63,7 +63,7 @@ export function surveyEmail(managerDNS: string) {
           </tr> <!-- HEADER 1 -->
           <tr style="background: #fff;">
             <td align="center"> <a href="#" target="_blank" style="text-decoration:none;"><img
-                  src="http://drive.google.com/uc?export=view&id=11nScspSzZX1WN9U957285Fkm9pZ54dQd" width="230" height="auto" style="display:block; margin-bottom: 25px;"
+                  src="https://dapp.bot/Edu.svg" width="230" height="auto" style="display:block; margin-bottom: 25px;"
                   border="0" alt="Eximchain" title="Eximchain" /></a>
               <h2
                 style="font-size: 1.34em; font-weight: bold; margin-top: -10px; margin-bottom: 0.5em; color: #515762;">
